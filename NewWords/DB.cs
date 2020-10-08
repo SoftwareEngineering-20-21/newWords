55ba78c49827f327322b1b3290304ec157413fc5
Володимир Параняк <paranyakv83@gmail.com>
2020-10-08 23:04:55 +0300

    Add database classes and migrations

---
+++ NewWords/DB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewWords
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<LanguageWords> Dictionary { get; set; }
+
+        public DbSet<Word> Words { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=wordsApp.db");
+    }
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+
+    }
+    public class Language
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+    }
+    public class Word
+    {
+        public int id { get; set; }
+    }
+    public class LanguageWords
+    {
+        public int id { get; set; }
+        public int langId { get; set; }
+        public string definition { get; set; }
+        public string word { get; set; }
+        public int word_id { get; set; }
+    }
+}
