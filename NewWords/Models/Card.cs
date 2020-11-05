0afaf7023c06c3aca268e3962deacafd854cf611
Володимир Параняк <paranyakv83@gmail.com>
2020-11-06 01:33:41 +0200

    Add card class

---
+++ NewWords/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewWords.Models
+{
+    class Card
+    {
+        Word Origin { get; set; }
+        List<Word> Translates { get; set; }
+        int Correct;
+    }
+}
