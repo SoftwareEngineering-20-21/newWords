db495bd2de3adcf69ae0baa9f86f0b01de3d14b9
shyshach <shymchak198@gmail.com>
2020-12-16 21:54:16 +0200

    ss

---
+++ NewWords/Models/GameContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewWords.Models
+{
+    class GameContext
+    {
+        AppContext AppContext = new AppContext(); 
+        int Counter { get; set; }
+        int RightAnswersCounter { get; set; }
+        int CurrentCardId { get; set; }
+
+        int Correct;
+        public int GetCurrentSuccess(){
+            return this.RightAnswersCounter;
+            }
+        public void Guess() {
+            this.RightAnswersCounter += 1;
+        }
+        public List<Word> GenerateCards()
+        {
+            var words = new List<Word>;
+            using (var context = new AppContext())
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    words.Add(context.Words.ToList()[i]);
+                }
+                return words;
+            }
+        }
+
+    }
+}
